820f339babfb487e0f0b5b682edf0a0e244d4ea1
devksh930 <ksh930@icloud.com>
2022-01-04 08:25:26 +0900

    init : oEmbed프로젝트

---
+++ src/main/java/me/devksh930/oembed/SpringOembedAppApplication.java
@@ -0,0 +1,13 @@
+package me.devksh930.oembed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOembedAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOembedAppApplication.class, args);
+    }
+
+}
