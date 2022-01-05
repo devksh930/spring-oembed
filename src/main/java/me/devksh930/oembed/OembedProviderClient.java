4c51a86a1d4066082ecf239fe71176407f68be47
devksh930 <ksh930@icloud.com>
2022-01-05 12:09:11 +0900

    test: OembedProviderClient - getProvider 테스트코드 추가#1

--- src/main/java/me/devksh930/oembed/OembedProviderClient.java
+++ src/main/java/me/devksh930/oembed/OembedProviderClient.java
@@ -1,23 +1,26 @@
 package me.devksh930.oembed;
 
-import lombok.RequiredArgsConstructor;
 import me.devksh930.oembed.dto.OembedProviderDto;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Component
-@RequiredArgsConstructor
+@Service
 public class OembedProviderClient {
 
     private final RestTemplate restTemplate;
 
     private static final String OEMBED_PROIVDER_URL = "https://oembed.com/providers.json";
+
+    public OembedProviderClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     public List<OembedProviderDto> getProvider() {
 
