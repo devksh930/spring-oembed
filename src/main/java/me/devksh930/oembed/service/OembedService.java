ac9becc6d16ad1c9c7387141c44941a787bae42d
devksh930 <ksh930@icloud.com>
2022-01-08 12:03:54 +0900

    FIX : 프로바이더들이 정의되지 않은 리소스를 넘길때 #7

--- src/main/java/me/devksh930/oembed/service/OembedService.java
+++ src/main/java/me/devksh930/oembed/service/OembedService.java
@@ -3,8 +3,9 @@ package me.devksh930.oembed.service;
 import lombok.RequiredArgsConstructor;
 import me.devksh930.oembed.client.OembedClient;
 import me.devksh930.oembed.dto.EndpointsDto;
-import me.devksh930.oembed.dto.OembedDto;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +13,7 @@ public class OembedService {
     private final OembedProviderService oembedProviderService;
     private final OembedClient oembedClient;
 
-    public OembedDto getOembedResource(String url) {
+    public Map<String, Object> getOembedResource(String url) {
         EndpointsDto byUrlPathMatching = oembedProviderService.findByUrlPathMatching(url);
         return oembedClient.getOembedResource(url, byUrlPathMatching.getUrl());
     }
