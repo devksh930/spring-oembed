0ce4f87b3e34742ddc4ca6e90709b4c9ff6f2b06
devksh930 <ksh930@icloud.com>
2022-01-10 05:10:25 +0900

    FIX : 인스그램과 페이스북에 대한 예외처리#11

--- src/main/java/me/devksh930/oembed/client/OembedClient.java
+++ src/main/java/me/devksh930/oembed/client/OembedClient.java
@@ -1,6 +1,8 @@
 package me.devksh930.oembed.client;
 
 import lombok.RequiredArgsConstructor;
+import me.devksh930.oembed.exception.ClientForbiddenException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -20,18 +22,38 @@ public class OembedClient {
     private final RestTemplate restTemplate;
 
 
+    @Value("${oembed.facebook.accessToken}")
+    private String facebookAccessToken;
+    @Value("${oembed.instagram.accessToken}")
+    private String instagramAccessToken;
+
     private RequestEntity<Void> requestEntity(URI uri) {
         return RequestEntity
                 .get(uri)
                 .build();
     }
 
-    public Map<String, Object> getOembedResource(String url, String apiUrl) {
-        URI reqeustURL = UriComponentsBuilder.fromHttpUrl(apiUrl)
-//                .queryParam("format", "json")
-                .queryParam("url", url).encode().build().toUri();
+    private URI makeUri(String url, String apiUrl) {
+        UriComponentsBuilder requestURLBuilder = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                .queryParam("url", url)
+                .queryParam("format", "json");
+        URI requestURL;
 
-        ResponseEntity<HashMap<String, Object>> exchange = restTemplate.exchange(requestEntity(reqeustURL), new ParameterizedTypeReference<>() {
+        if (apiUrl.contains("instagram") || apiUrl.contains("facebook")) {
+            throw new ClientForbiddenException("권한 문제로 지원하지 않는 URL입니다");
+//            requestURL = requestURLBuilder
+//                    .queryParam("access_token", instagramAccessToken).encode().build().toUri();
+
+        } else {
+            requestURL = requestURLBuilder
+                    .encode().build().toUri();
+        }
+        return requestURL;
+    }
+
+    public Map<String, Object> getOembedResource(String url, String apiUrl) {
+
+        ResponseEntity<HashMap<String, Object>> exchange = restTemplate.exchange(requestEntity(makeUri(url, apiUrl)), new ParameterizedTypeReference<>() {
         });
         return exchange.getBody();
     }
